3f42ebed22d9bfbe359a125d5d8ff7154cfc4265
Hamed Mohammadi <hamed@dehongi.com>
2024-02-26 22:45:39 +0330

    add to cart

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
+
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+
 
 # Create your views here.
 
